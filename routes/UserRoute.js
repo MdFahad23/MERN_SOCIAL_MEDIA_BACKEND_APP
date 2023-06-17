291a1f63826b3ca5e402f9177b1c337885cf034d
Md Fahad <fahadbdctglh8@gmail.com>
2023-06-17 09:23:00 +0600

    User update delete follow unfolow

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { GetUser } from "../controller/UserController.js";
+import {
+  GetUser,
+  UnFollowUser,
+  deleteUser,
+  followUser,
+  updateUser,
+} from "../controller/UserController.js";
 
 const router = express.Router();
 
 router.get("/:id", GetUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+router.put("/:id/follow", followUser);
+router.put("/:id/unfollow", UnFollowUser);
 
 export default router;
