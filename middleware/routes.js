e5813ed8d109cbe73c35d24c412cfa49aaee64fb
Md Fahad <fahadbdctglh8@gmail.com>
2023-06-19 14:03:42 +0600

    postController  CRUD operation declear

--- middleware/routes.js
+++ middleware/routes.js
@@ -1,7 +1,9 @@
 import AuthRoute from "../routes/AuthRoute.js";
 import UserRoute from "../routes/UserRoute.js";
+import PostRoute from "../routes/PostRoute.js";
 
 export default (app) => {
   app.use("/auth", AuthRoute);
   app.use("/user", UserRoute);
+  app.use("/post", PostRoute);
 };
